e1cb2627830c7c9712039b36f9e15bfb02fa5941
Vineet Kumar <vineet.rke@gmail.com>
2023-03-19 22:34:59 +0530

    added for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,14 @@
-
-fetch('https://puzzle.mead.io/puzzle').
-    then((response) => {
-        response.json().
-            then((data) => {
-                console.log(data)
-            })
-    });
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const address_v = document.querySelector('#address')
 const location_v = document.querySelector('#location')
 const forecast_v = document.querySelector('#forecast')
+
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
-    fetch(`http://localhost:3000/weather?address=${location}`).
+    
+    fetch(`/weather?address=${location}`).
         then((response) => {
             response.json().
                 then((data) => {
